ea104b44aa19bde3cf2c4e1af7a5f31107a32a07
seshadevlo <l.vet.operaciones@gmail.com>
2025-11-05 02:11:36 -0300

    v1.0 base de proyecto sin avances

---
+++ src/main/java/com/estudiantes/apirest/ApirestfullApplication.java
@@ -0,0 +1,13 @@
+package com.estudiantes.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfullApplication.class, args);
+	}
+
+}
